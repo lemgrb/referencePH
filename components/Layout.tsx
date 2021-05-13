e27866b3a2bd440710c976a1d89aa7accd48c471
Lemuel Bulangis <lemgrb@gmail.com>
2021-05-13 23:05:59 +0800

    One line update

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -59,7 +59,7 @@ const Layout = ({ children, title = 'ReferencePH', home = false}: Props) => (
       </div>
       <div className="col-md-8">
         <h5>Hey!</h5>
-        <p>If you have a feature request or found an issue, please <a href="https://github.com/lemgrb/referencePH/issues/new/choose" target="_blank">submit an issue</a></p>
+        <p>If you have a feature request or found an issue, please contact us on our Twitter and Facebook pages.</p>
         <p>If you like this site maybe you can <a href="https://www.buymeacoffee.com/lemuelb">Buy me a coffee</a>? <EmojiSmile /></p>
         <span>&copy; 2021 ReferencePH Version 0.0.5</span>
       </div>
